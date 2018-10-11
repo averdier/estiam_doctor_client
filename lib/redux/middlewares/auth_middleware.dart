4e4fac64ba27c7d2a291fd95552fe9a6c68dc002
Rémi LARDIER <remi0411@hotmail.fr>
2018-10-11 14:42:05 +0200

    Fix pb with kebab.dart

--- lib/redux/middlewares/auth_middleware.dart
+++ lib/redux/middlewares/auth_middleware.dart
@@ -1,3 +1,4 @@
+import 'package:estiam_doctor_client/views/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:estiam_doctor_client/models/user.dart';
 import 'package:estiam_doctor_client/redux/actions/auth_actions.dart';
@@ -10,9 +11,11 @@ import 'package:google_sign_in/google_sign_in.dart';
 /// Create and return auth middleware
 List<Middleware<AppState>> createAuthMiddleware() => <Middleware<AppState>>[
   new TypedMiddleware<AppState, UserLoginRequest>(logIn),
+  new TypedMiddleware<AppState, UserLogout>(logOut),
 ];
 
 final logIn = _login();
+final logOut = _logOut();
 
 /// Login user
 Middleware<AppState> _login() {
@@ -42,6 +45,32 @@ Middleware<AppState> _login() {
             .pushNamedAndRemoveUntil('/main', (_) => false);
       } catch (error) {
         store.dispatch(new UserLoginFailure(error));
+      }
+    }
+  };
+}
+
+Middleware<AppState> _logOut() {
+  return (Store store, action, NextDispatcher next) async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    GoogleSignIn _googleSignIn = new GoogleSignIn();
+    if (action is UserLogout) {
+      try {
+
+        _googleSignIn.signOut();
+        await _auth.signOut();
+        store.dispatch(new LogOutSuccessful());
+
+        var route = new MaterialPageRoute(
+            settings: new RouteSettings(name: '/login'),
+            builder: (context) => new LoginScreen());
+
+        Navigator
+            .of(action.context)
+            .pushAndRemoveUntil(route, ModalRoute.withName('/'));
+        print('logging out...');
+      } catch (error) {
+        print(error);
       }
     }
   };
