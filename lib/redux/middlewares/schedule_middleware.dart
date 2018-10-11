00dd49be94a88fab14b4d2492220f8dace503b37
Arthur <a.verdier@outlook.fr>
2018-10-11 17:21:52 +0200

    fix

--- lib/redux/middlewares/schedule_middleware.dart
+++ lib/redux/middlewares/schedule_middleware.dart
@@ -19,7 +19,7 @@ final doSchedule = _doSchedule();
 Middleware<AppState> _getSchedules() {
   return (Store store, action, NextDispatcher next) async {
     if (action is ScheduleListRequest) {
-      QuerySnapshot querySnapshot = await Firestore.instance.collection("reservations").where('userId', isEqualTo: action.doctorId).getDocuments();
+      QuerySnapshot querySnapshot = await Firestore.instance.collection("reservations").where('doctorId', isEqualTo: action.doctorId).getDocuments();
       var list = querySnapshot.documents;
       store.dispatch(ScheduleListRequestSuccess(list.map((i) => new Schedule.fromSnapshot(i)).toList()));
     }
@@ -28,12 +28,10 @@ Middleware<AppState> _getSchedules() {
 
 Middleware<AppState> _doSchedule() {
   return (Store store, action, NextDispatcher next) async {
-    print('========================================================');
     if (action is ScheduleRequest) {
-      QuerySnapshot querySnapshot = await Firestore.instance.collection("reservations").orderBy('reservationDateTime').startAt([action.dateTime.add(new Duration(minutes: -30))]).endAt([action.dateTime.add(new Duration(minutes: 30))]).where('userId', isEqualTo: action.doctorId).getDocuments();
+      QuerySnapshot querySnapshot = await Firestore.instance.collection("reservations").orderBy('reservationDateTime').startAt([action.dateTime.add(new Duration(minutes: -30))]).endAt([action.dateTime.add(new Duration(minutes: 30))]).where('doctorId', isEqualTo: action.doctorId).getDocuments();
 
       var list = querySnapshot.documents.map((i) => new Schedule.fromSnapshot(i)).toList();
-      print('========================================================');
       if (list.length == 0) {
         var schedule = new Schedule(action.user.id, action.user.name, action.user.phone, action.doctorId, action.dateTime);
         Firestore.instance.collection('reservations').document().setData(schedule.toJSON());
