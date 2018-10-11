d6e56f8491b6edf9b66f06a62c0e2134c6ed3e70
Sullivan <Sullivan.fabre@gmail.com>
2018-10-11 16:48:07 +0200

    Update doctor list screen

--- lib/views/doctor_list_screen.dart
+++ lib/views/doctor_list_screen.dart
@@ -15,44 +15,64 @@ class DoctorListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('FindADoc')
+          title: new Text('Choisir un docteur')
       ),
       body: new StoreConnector<AppState, _ViewModel>(
-        converter: (Store<AppState> store) {
-          _ViewModel vm = _ViewModel.fromStore(store);
-          return vm;
-        },
-        builder: (BuildContext context, vm) {
-          return _buildDoctorList(context, vm.doctors);
-        }
+          converter: (Store<AppState> store) {
+            _ViewModel vm = _ViewModel.fromStore(store);
+            return vm;
+          },
+          builder: (BuildContext context, vm) {
+            return _buildDoctorList(context, vm.doctors);
+          }
       ),
       drawer: new MainDrawer(),
     );
   }
-  
+
   /// Create kebabs list
   Widget _buildDoctorList(BuildContext context, List<Doctor> doctors) {
-    return ListView.builder(
-      itemCount: doctors.length,
-      itemBuilder: (context, i) {
-        return _buildDoctorRow(context, doctors.elementAt(i));
-      },
+    return Container(
+      decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.blue,
+                Colors.white70,
+              ])),
+        child: ListView.builder(
+          itemCount: doctors.length,
+          itemBuilder: (context, i) {
+            return _buildDoctorRow(context, doctors.elementAt(i));
+          },
+        ),
     );
   }
-  
+
   /// Create row from kebab
   Widget _buildDoctorRow(BuildContext context, Doctor doctor) {
-    return ListTile(
-      title: new Text(doctor.name),
-      subtitle: new Text(doctor.officeName.toString()),
-      onTap: () {
-        print(doctor.toString() + ' tapped');
-        StoreProvider.of<AppState>(context).dispatch(
-          new DoctorRequest(doctor.name, doctor.officeName, doctor.phoneNumber, doctor.userId)
-        );
-        Navigator.of(context).pushNamed('/doctors/details');
-      }
+    return InkWell(
+        onTap: () {
+          StoreProvider.of<AppState>(context).dispatch(
+              new DoctorRequest(doctor.name, doctor.officeName, doctor.phoneNumber, doctor.userId)
+          );
+          Navigator.of(context).pushNamed('/doctors/details');
+        },
+        child: Card(
+            child: new Container(
+              padding: EdgeInsets.all(8.0),
+              child: new Row(
+                children: <Widget>[
+                  CircleAvatar( child: new Text(doctor.name[0]),),
+                  Padding(padding: EdgeInsets.only(right: 10.0)),
+                  Text("Dr. " + doctor.name, style: TextStyle(fontSize: 20.0),),
+                ],
+              ) ,
+            ),
+          )
     );
+
   }
 }
 
