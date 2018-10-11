7189a6b5278fa0aef3c7d9679fd383da8ae7d8f5
Sullivan <Sullivan.fabre@gmail.com>
2018-10-11 16:00:50 +0200

    Appointment update with two date inputs

--- lib/views/doctor_appointment_screen.dart
+++ lib/views/doctor_appointment_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:estiam_doctor_client/views/components/main_drawer.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:datetime_picker_formfield/time_picker_formfield.dart';
+
 
 void main() => runApp(new DoctorAppointmentScreen());
 
@@ -26,6 +30,8 @@ class DoctorAppointmentScreenHome extends StatefulWidget {
 
 // Page détail d'un docteur
 class _DoctorAppointmentScreenState extends State<DoctorAppointmentScreenHome> {
+  final dateFormat = DateFormat("EEEE, MMMM d, yyyy 'at' h:mma");
+  final timeFormat = DateFormat("h:mm a");
 
   @override
   Widget build(BuildContext context) {
@@ -49,22 +55,40 @@ class _DoctorAppointmentScreenState extends State<DoctorAppointmentScreenHome> {
                     ),
                     Padding(
                       padding: const EdgeInsets.only(top: 30.0),
-                      child: Text("Sélectionner l'horaire", style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
+                      child: Text("Choisir le créneau", style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
                     ),
                     Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: DropdownButton<String>(
-                          //https://stackoverflow.com/questions/46323682/how-to-get-selected-index-of-dropdownbutton-in-flutter
-                          hint: new Text("Horaire(s) disponible(s)"),
-                          items: <String>['A', 'B', 'C', 'D'].map((String value) {
-                            return new DropdownMenuItem<String>(
-                              value: value,
-                              child: new Text(value),
-                            );
-                          }).toList(),
-                          //voir lien stackoverflow
-                          onChanged: (_) {},
-                        )
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Text("Jour", style: new TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                    DateTimePickerFormField(
+                      format: dateFormat,
+                      onChanged: (date) {
+                        Scaffold
+                            .of(context)
+                            .showSnackBar(SnackBar(content: Text('$date')));
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Text("Horaire", style: new TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                    TimePickerFormField(
+                      format: timeFormat,
+                      onChanged: (time) {
+                        Scaffold
+                            .of(context)
+                            .showSnackBar(SnackBar(content: Text('$time')));
+                      },
+                    ),
+
+                    DateTimePickerFormField(
+                      format: dateFormat,
+                      enabled: false,
+                    ),
+                    TimePickerFormField(
+                      format: toDateFormat(TimeOfDayFormat.HH_colon_mm),
+                      enabled: false,
                     ),
                     Padding(
                         padding: const EdgeInsets.only(top: 20.0),
